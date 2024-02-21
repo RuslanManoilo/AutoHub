84d072b53e977abc582b67e9240ad86f32463c14
Ruslan <manoiloruslan@gmail.com>
2024-02-21 23:21:42 +0200

    initial settings

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,10 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { Layout } from './Layout/Layout';
+import { lazy } from 'react';
 
-import Home from 'pages/Home/Home';
-import Favorite from 'pages/Favorite/Favorite';
-import Catalog from 'pages/Catalog/Catalog';
-
-// import { lazy } from 'react';
-// const Home = lazy(() => import('../pages/Home/Home'));
+const Home = lazy(() => import('../pages/Home/Home'));
+const Catalog = lazy(() => import('../pages/Catalog/Catalog'));
+const Favorite = lazy(() => import('../pages/Favorite/Favorite'));
 
 export const App = () => {
   return (
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/AutoHub">
+    <BrowserRouter basename="/RuslanManoilo/AutoHub">
       <App />
     </BrowserRouter>
   </React.StrictMode>
